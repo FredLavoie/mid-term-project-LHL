cfc835a6d64dec843a773a1b3018a400b73887ea
FredLavoie <flavoie3@gmail.com>
2019-06-20 00:09:18 +0000

    Initiate project

---
+++ db/seeds/a_users.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex, Promise) {
+  return Promise.all([
+    knex('users').del(),
+    knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1'),
+    knex('users').then(function() {
+      return Promise.all([
+        knex('users').insert({ name: 'Alice' }),
+        knex('users').insert({ name: 'Bob' }),
+        knex('users').insert({ name: 'Charlie' }),
+      ]);
+    }),
+  ]);
+};
