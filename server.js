11b92cbff2e01fdcbb8bb680e5828fe38f7bc35e
MattHughson! <matt@koodomasson.com>
2019-06-20 17:12:51 +0000

    completed html for add map

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
-});
+  console.log("Example app listening on port " + PORT)
+});  
