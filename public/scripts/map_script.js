e56bd16b002b8a5d69fbfe3021154a79ec8c194c
keefu <cyuplia@gmail.com>
2019-06-21 22:30:21 +0000

    Create Homepage HTML

--- public/scripts/map_script.js
+++ public/scripts/map_script.js
@@ -2,13 +2,14 @@
 
 function initMap() {
 
-    var uluru = {lat: -25.344, lng: 131.036};
+  var uluru = { lat: -25.344, lng: 131.036 };
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
     position: uluru,
     map: map
-  });}
+  });
+}
